3c5909ae52d46094bc23ef19a1f018446c972a4c
Xaviontt <xavion@list.ru>
2023-06-17 11:20:43 +0300

    Удаление готово

--- src/main/java/com/itmo/blps/delegators/DeleteTopic.java
+++ src/main/java/com/itmo/blps/delegators/DeleteTopic.java
@@ -1,14 +1,35 @@
 package com.itmo.blps.delegators;
 
+import com.itmo.blps.dao.TopicRepository;
+import com.itmo.blps.service.TopicService;
+import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.feel.syntaxtree.In;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.inject.Named;
+
+@Named("deleteTopic")
 public class DeleteTopic implements JavaDelegate {
+    @Autowired
+    TopicRepository topicRepository;
+    @Autowired
+    TopicService topicService;
+
     @Override
     public void execute(DelegateExecution execution) throws Exception {
-        long topicId = (long) execution.getVariable("topicId");
+        Integer topicIdTmp = (Integer) execution.getVariable("topicId");
+        Long topicId = Long.valueOf(topicIdTmp);
 
-        if (topicId)
+        if (!topicRepository.existsById(topicId)) {
+            throw new BpmnError(
+                    "topic_not_found",
+                    "no topic found with this id"
+            );
+        }
+
+        topicService.delete(topicId);
 
     }
 }
